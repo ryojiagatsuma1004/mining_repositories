374aeada0869350f8c6fafae9115b2c91feeb72b
ryojiagatsuma1004 <agatsuma.ryoji.as9@naist.ac.jp>
2024-05-22 17:51:33 +0900

    change: requirements.txtから依存パッケージを解決するように変更

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@
 
 from setuptools import setup, find_packages
 
+
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f if line and not line.startswith('#')]
+
+
 setup(
     name='mining_repositories',
     version='0.1',
     packages=find_packages(include=['mining_repositories', 'mining_repositories.*']),
-    install_requires=[
-        'PyGithub'
-    ],
-    tests_require=[
-        'unittest'
-    ],
+    install_requires=parse_requirements('requirements.txt'),
     test_suite='tests',
     python_requires='>=3.6',
 )
